5bf0a69b8efb49c31d80c0c9bf0605dd26e9ac7c
Yasas Rajika <it22324542@my.sliit.lk>
2025-03-22 10:29:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/")
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+
+  return (
+    <>
+      <div>Yasas</div>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <h1>{message}</h1>;
+
+    </>
+  )
+}
+
+export default App
