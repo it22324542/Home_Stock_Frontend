7e43cffeafcbd3b2f97811fd72a8f99571b4b413
Yasas Rajika <it22324542@my.sliit.lk>
2025-04-10 21:39:16 +0530

    current inventory frontend complete

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5175, 
+    strictPort: true, 
+    host: 'localhost',
     proxy: {
       "/api": {
         target: "http://localhost:5000",
